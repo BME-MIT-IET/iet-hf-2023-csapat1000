8182b779df35c3a7a6e2e7647817b088808277ec
jud11t <kovacsj2002@gmail.com>
2023-06-01 19:19:36 +0200

    JUnit5 és Maven Surefire Plugin hozzáadása #5

---
+++ src/test/java/TestPlugin.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPlugin {
+    @Test
+    void justAnExample() {
+        assertEquals(2 + 2, 4);
+    }
+}
