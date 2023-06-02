e1216705f6e6dceb4d8158211dc19b32347849f1
Katinka666 <100243266+Katinka666@users.noreply.github.com>
2023-06-02 03:05:49 +0200

    Dokumentacio elkeszitese

---
+++ src/main/java/Testing.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import javax.swing.*;
+
+public class Testing {
+
+
+    @BeforeClass
+    public void setup(){
+    }
+
+    @Test
+    public void testMethod(){
+    }
+
+}
